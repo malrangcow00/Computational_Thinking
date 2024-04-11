d933bc4d8d7025e7498abee96f240ca8e69d25e3
MalrangCow <malrangcow00@gmail.com>
2024-04-11 09:36:06 +0900

    0411_TIL

---
+++ Java/BAEKJOON/BOJ_1000.java
@@ -0,0 +1,21 @@
+package ch01.sec01.BAEKJOON;
+
+public class BOJ_1000 {
+
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            System.out.println(A + B);
+
+            scanner.close();
+        }
+    }
+
+}
---
+++ Java/BAEKJOON/BOJ_2557_Hello_World.java
@@ -0,0 +1,7 @@
+package Algorithm_Java.BAEKJOON;
+
+public class BOJ_2557_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
